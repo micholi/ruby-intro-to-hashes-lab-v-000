e355b1c728e3cf28fe5b13f824fa538849d85669
Michelle Olivieri <michelleo72@gmail.com>
2018-03-22 14:07:58 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,18 +8,18 @@ end
 
 def monopoly
 	monopoly = {
-		:railroads => {
-			:pieces => 4
-			:names => {}
-			:rent_in_dollars => {}
-		}
+		:railroads => {}
 	}
 
 end
 
 def monopoly_with_second_tier
 	monopoly = {
-		:railroads => {}
+		:railroads => {
+			:pieces => 4
+			:names => {}
+			:rent_in_dollars => {}
+		}
 	}
 
 end
